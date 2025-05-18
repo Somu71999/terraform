6063812e9d4c60ca275d4421c8d2631b9e37840b
Somu71999 <veluri99930@gmail.com>
2025-05-18 15:40:42 +0530

    updated the workflow

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
+#provider "aws" {
+#  region = "us-east-1"
+#}
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI in us-east-1
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "example" {
+#  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI in us-east-1
+#  instance_type = "t2.micro"
+#}
